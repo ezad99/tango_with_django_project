2d53966cf81c9e66912153cec033573bc7907e54
Ezad Zamily <2680737E@student.gla.ac.uk>
2023-01-28 10:25:03 +0000

    Chapter 6, 6.3 Creating a Details Page Completed

--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     likes = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    #args is just a name in can be replaced, what's important is the unpacking operator(*)
+    #args accepts positional arguments
+    #kwargs accepts keyword/named arguments
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 from django.http import HttpResponse
 
 
@@ -23,3 +24,25 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Ezad'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    #Create a context dictionary which we can pass
+    # to the template rendering engine
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception
+        # The .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
