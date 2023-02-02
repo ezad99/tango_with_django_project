d78ba60e1527eb4c70fb6b8b5ffc7ddb4a172d4c
Ezad Zamily <2680737E@student.gla.ac.uk>
2023-02-02 13:41:50 +0000

    Chapter 7, 7.2 Page and Category Forms Completed

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,9 @@ from rango.models import Category
 from rango.models import Page
 from django.http import HttpResponse
 
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -48,3 +51,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    #A HTTP Post?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            cat = form.save(commit=True)
+            print(cat, cat.slug)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
